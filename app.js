207999e7fbf1b0aff9671b4349ef443be10cad92
michaelnunezt <michaelnunezt@gmail.com>
2024-09-05 14:00:56 +0100

    finished

---
+++ app.js
@@ -0,0 +1,75 @@
+
+// // Exercise 1
+// const foods = [];  
+// console.log('Exercise 1 result:', foods);
+
+// // Exercise 2
+// let foods = [];
+// foods.unshift('pizza');
+// foods.splice(1,0, 'cheeseburger');
+
+// console.log('Exercise 2 result:', foods);
+
+// Exercise 3
+// let foods = ['pizza', 'cheeseburger'];
+// foods.unshift('taco');
+// console.log('Exercise 3 result:', foods);
+
+// Exercise 4
+// let foods = ['taco', 'pizza', 'cheeseburger'];
+// let favFood = foods[1];
+
+// console.log('Exercise 4 result:', favFood);
+
+// Exercise 5
+// let foods = ['taco', 'pizza', 'cheeseburger'];
+// foods.splice(2, 0, 'tofu');
+
+// console.log('Exercise 5 result:', foods);
+
+// Exercise 6
+// let foods = ['taco', 'pizza', 'tofu', 'cheeseburger'];
+// foods.splice(1, 1, 'sushi', 'cupcake');
+
+// console.log('Exercise 6 result:', foods);
+
+// Exercise 7
+// let foods =  [ 'taco', 'sushi', 'cupcake', 'tofu', 'cheeseburger' ];
+// let yummy = foods.slice(1, 3);
+
+// console.log('Exercise 7 result:', yummy);
+
+// Exercise 8
+// let foods = ['taco', 'sushi', 'cupcake', 'tofu', 'cheeseburger'];
+// let soyIdx = foods.indexOf('tofu');
+
+// console.log('Exercise 8 result:', soyIdx);
+
+// Exercise 9
+// const foods = ['taco', 'sushi', 'cupcake', 'tofu', 'cheeseburger'];
+// let allFoods = foods.join(' -> ');
+
+// console.log('Exercise 9 result:', allFoods);
+
+// Exercise 10
+// const foods = ['taco', 'sushi', 'cupcake', 'tofu', 'cheeseburger'];
+// const hasSoup = foods.includes('soup');
+
+// console.log('Exercise 10 result:', hasSoup);
+
+// Exercise 11
+// const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+// let odds = [];
+
+// nums.forEach(num => {
+//   if (num % 2 !== 0) {
+//     odds.push(num);
+//   }
+// });
+
+
+// console.log('Exercise 11 result:', odds); 
+
+
+// Exercise 12
+
